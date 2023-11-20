b8170b2463002da52ac9a367bb134eee3ebea20b
MohammadHosein <48368146+MohammadHosein21@users.noreply.github.com>
2023-11-20 09:29:54 +0330

    Update chartBox.py

--- chartBox.py
+++ chartBox.py
@@ -6,7 +6,7 @@ def get_price():
     AAPL['Candle Type'] = ['Bullish' if close > open else 'Bearish' for close, open in zip(AAPL['Close'], AAPL['Open'])]
     AAPL.index = AAPL.index.strftime('%Y-%m-%d %I:%M:%S %p')
     AAPL.to_excel('AAPL.xlsx')
-    print(AAPL)
+    # print(AAPL)
 
 
 if __name__ == '__main__':
